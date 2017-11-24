b45bd1f2ce5f076061ef77066e416220025b3471
Dziana Rutkovskaya <dziana_rutkouskaya@epam.com>
2017-11-24 12:21:56 +0300

    TimeUtil: getCurrentDate(),getCurrentDatePlusAmountOfDays(int)

--- src/main/java/com/rzd/selenium/util/TimeUtil.java
+++ src/main/java/com/rzd/selenium/util/TimeUtil.java
@@ -5,18 +5,29 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class TimeUtil {
-    public static Calendar getCalendar(){
+
+    private static Calendar calendar = Calendar.getInstance();
+
+    public static Calendar getCalendar() {
         Calendar calendar = Calendar.getInstance();
 
         return calendar;
     }
+//Заменить метод getCalendar на те что написаны снизу!!!
+    public static String getCurrentDate(int plusDaysToCurrentDate) {
+        calendar.add(Calendar.DATE, plusDaysToCurrentDate);
+        Date dateNow = calendar.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d.MM.yyyy");
+        String date = dateFormat.format(dateNow);
+        return date;
+    }
 
-    public static String generateDate(int plusDaysToCurrentDate) {
-        Calendar cal = getCalendar();
-        cal.add(Calendar.DATE, plusDaysToCurrentDate);
-        Date dateNow = cal.getTime();
+    public static String getCurrentDatePlusAmountOfDays(int amountOfDays) {
+        calendar.add(Calendar.DATE, amountOfDays);
+        Date dateNow = calendar.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("d.MM.yyyy");
         String date = dateFormat.format(dateNow);
         return date;
     }
 }
+
