0c746f66820c7fac0fca18c16055f6ad88bcd8de
Dziana Rutkovskaya <dziana_rutkouskaya@epam.com>
2017-11-30 16:39:20 +0300

    refactor

--- src/main/java/ru/rzd/business_objects/User.java
+++ src/main/java/ru/rzd/business_objects/User.java
@@ -1,8 +1,10 @@
 package ru.rzd.business_objects;
 
+import ru.rzd.util.ConfigurationManager;
+
 public class User {
-    private static final String PASSWORD = "vfq2017";
-    private static final String LOGIN = "RZD_LABA";
+    private static final String PASSWORD = ConfigurationManager.getProperty("user.password");
+    private static final String LOGIN = ConfigurationManager.getProperty("user.login");
 
     public static String getPassword() {
         return PASSWORD;
